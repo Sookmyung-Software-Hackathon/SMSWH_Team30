0f486ecd266a613b3bd71057f3bf6add463c1c89
Julee <77472171+julee24@users.noreply.github.com>
2021-11-08 00:32:07 +0900

    Add files via upload

---
+++ ThoughtfulSnow/app/build/generated/source/buildConfig/release/com/team30/thoughtfulsnow/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.team30.thoughtfulsnow;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.team30.thoughtfulsnow";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
